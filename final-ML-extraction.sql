47049da96a1cadc0970ac4a8f476480d002e4475
PsychicDonkey <160005684+PsychicDonkey@users.noreply.github.com>
2024-10-12 12:37:35 +0200

    Update final-ML-extraction.sql

--- final-ML-extraction.sql
+++ final-ML-extraction.sql
@@ -1,1 +1,12 @@
+-- list of categories for ML treatment
 
+SELECT
+dim_category_translated
+FROM `aqueous-tesla-423708-b1.tiller.order_line`
+GROUP BY dim_category_translated
+
+-- list of names for ML treatment
+SELECT
+dim_name_translated
+FROM `aqueous-tesla-423708-b1.tiller.order_line`
+GROUP BY dim_name_translated
