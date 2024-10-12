37fb188fcd26af531facd1941731f0f5621ef5e0
PsychicDonkey <160005684+PsychicDonkey@users.noreply.github.com>
2024-10-12 12:42:35 +0200

    Update order_data_exploratory_queries.sql

--- order_data_exploratory_queries.sql
+++ order_data_exploratory_queries.sql
@@ -26,3 +26,19 @@ WHERE id_order = 38353338
 SELECT * 
 FROM `aqueous-tesla-423708-b1.tiller.order_data` 
 WHERE m_cached_payed-m_cached_price <> 0
+
+-- EDA on ORDER LINE
+
+-- check # of rows on specific order
+SELECT
+*
+FROM aqueous-tesla-423708-b1.tiller.order_line as order_line
+
+WHERE order_line.id_order =15967970;
+
+-- check sum revenue on specific order
+SELECT
+SUM(m_total_price_inc_vat)
+FROM aqueous-tesla-423708-b1.tiller.order_line as order_line
+
+WHERE order_line.id_order =15967970;
